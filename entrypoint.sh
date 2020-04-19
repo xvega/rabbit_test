0921b6f4182921c78716f7ff4282b86a19fa250d
xavi <xabi1309@gmail.com>
2020-04-19 12:12:15 +0200

    Adding migrations and seeding to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
+bundle exec rake db:create
+
+bundle exec rake db:migrate RAILS_ENV=test
+bundle exec rake db:migrate RAILS_ENV=development
+
+bundle exec rake db:seed RAILS_ENV=development
+
 exec "$@"
