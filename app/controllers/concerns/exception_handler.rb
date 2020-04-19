2e3fa436af05058c3299fddb29d1ca508b641848
xavi <xabi1309@gmail.com>
2020-04-19 19:59:48 +0200

    Add exception handler module

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,11 +3,11 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      json_response({ message: e.message }, status: :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ message: e.message }, status: :unprocessable_entity)
     end
   end
 end
