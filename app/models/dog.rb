0a8f069cfe0de8dbf26a1d2c5c13f4b60f4f29a2
xavi <xabi1309@gmail.com>
2020-04-19 20:06:44 +0200

    Add meta data method and scopes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,17 @@ class Dog < ApplicationRecord
   belongs_to :breed
 
   validates :name, :age, presence: true
+
+  scope :total_by_breed, -> { joins(:breed).group('breeds.name').count }
+  scope :age_avg, -> { average(:age) }
+  scope :weight_avg, -> { average(:weight) }
+  scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
+
+  def self.summarized_data
+    {
+      total_by_breed: total_by_breed,
+      age_avg: age_avg,
+      weight_avg: weight_avg
+    }
+  end
 end
