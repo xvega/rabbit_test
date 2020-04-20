28c3d8e2a1dfa58865b97829917f4e54b85fd610
xavi <xabi1309@gmail.com>
2020-04-20 09:02:22 +0200

    Add API authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   include Response
   include ExceptionHandler
 
+  before_action :authenticate
+
   respond_to :json
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      @auth = ApiKey.exists?(access_token: token)
+    end
+  end
 end
